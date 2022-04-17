1e0ad564aad2e641fee0cb233c22e53af51e4ca5
aries <magesvijay2000@gmail.com>
2022-04-17 13:35:06 +0530

    add env and gitignore files

--- quizzeler/quizzeler/settings.py
+++ quizzeler/quizzeler/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3tnoi(4ud#*4k#!jr3a8#2qvu4m+(azvfqpprfnh8+wm%-or30'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
